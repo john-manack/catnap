a4046fad7de584fb28aef80056a06af26bbbc36b
John Manack <jmnck28@gmail.com>
2021-03-08 14:45:49 -0500

    establishes redux files

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import { updater } from './reducers';
+
+const defaultState = {
+    activity: 'napping',
+}
+
+export const store = createStore(
+    updater, 
+    defaultState, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
